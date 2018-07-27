e9089af84a4fe549a316df6892a9eaca11ce2938
Yuto.K <kairi3108@i.softbank.jp>
2018-07-28 00:29:21 +0900

    initcommit

---
+++ RSSTestReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSTestReader
+//
+//  Created by Yuto on 2018/07/28.
+//  Copyright © 2018年 Yuto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RSSTestReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RSSTestReader
+//
+//  Created by Yuto on 2018/07/28.
+//  Copyright © 2018年 Yuto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
